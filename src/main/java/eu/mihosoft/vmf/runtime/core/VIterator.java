7aa8af2081e6a8e4e48df4e1e0b3865824109bfc
Michael Hoffer <info@michaelhoffer.de>
2017-01-30 20:23:30 +0100

    v-iterator can be returned as stream

--- src/main/java/eu/mihosoft/vmf/runtime/core/VIterator.java
+++ src/main/java/eu/mihosoft/vmf/runtime/core/VIterator.java
@@ -5,12 +5,15 @@
  */
 package eu.mihosoft.vmf.runtime.core;
 
-
 import eu.mihosoft.vcollections.VList;
 
 import java.util.IdentityHashMap;
 import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.Stack;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Iterator that an iterates over the specified object graph.
@@ -37,13 +40,29 @@ public class VIterator implements Iterator<VObject> {
     }
 
     /**
+     * Returns a stream backed by this iterator.
+     *
+     * @return a stream backed by this iterator
+     */
+    public Stream<VObject> asStream() {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(
+                        iterator,
+                        Spliterator.ORDERED),
+                false);
+    }
+
+    /**
      * Returns an iterator that iterates over the specified object graph.
      *
      * @param root object graph to iterate
      * @return an iterator that iterates over the specified object graph
      */
     public static VIterator of(VObject root) {
-        return new VIterator(new VMFIterator((eu.mihosoft.vmf.runtime.core.internal.VObjectInternal) root));
+        return new VIterator(
+                new VMFIterator(
+                        (eu.mihosoft.vmf.runtime.core.internal.VObjectInternal) root)
+        );
     }
 }
 
@@ -53,7 +72,9 @@ public class VIterator implements Iterator<VObject> {
  * @author Michael Hoffer (info@michaelhoffer.de)
  */
 @SuppressWarnings("deprecation")
-class VMFIterator implements Iterator<eu.mihosoft.vmf.runtime.core.internal.VObjectInternal> {
+class VMFIterator
+        implements
+        Iterator<eu.mihosoft.vmf.runtime.core.internal.VObjectInternal> {
 
     private final IdentityHashMap<Object, Object> identityMap
             = new IdentityHashMap<>();
@@ -69,7 +90,8 @@ class VMFIterator implements Iterator<eu.mihosoft.vmf.runtime.core.internal.VObj
         return DEBUG;
     }
 
-    public VMFIterator(eu.mihosoft.vmf.runtime.core.internal.VObjectInternal root) {
+    public VMFIterator(
+            eu.mihosoft.vmf.runtime.core.internal.VObjectInternal root) {
         first = root;
         currentIterator = new VMFPropertyIterator(identityMap, root);
     }
@@ -95,7 +117,8 @@ class VMFIterator implements Iterator<eu.mihosoft.vmf.runtime.core.internal.VObj
             first = null;
         } else {
             // obtain next element from current iterator
-            n = (eu.mihosoft.vmf.runtime.core.internal.VObjectInternal) getCurrentIterator().next();
+            n = (eu.mihosoft.vmf.runtime.core.internal.VObjectInternal)
+                    getCurrentIterator().next();
 
             // - if element was not visited before then mark it as visited
             // - push the current iterator and create a new visitor that
@@ -109,7 +132,8 @@ class VMFIterator implements Iterator<eu.mihosoft.vmf.runtime.core.internal.VObj
                 identityMap.put(nIdentityObj, null);
                 iteratorStack.push(currentIterator);
                 currentIterator = new VMFPropertyIterator(
-                        identityMap, (eu.mihosoft.vmf.runtime.core.internal.VObjectInternal) n);
+                        identityMap, 
+                        (eu.mihosoft.vmf.runtime.core.internal.VObjectInternal) n);
             }
 
         }
@@ -119,11 +143,12 @@ class VMFIterator implements Iterator<eu.mihosoft.vmf.runtime.core.internal.VObj
 
     /**
      * Unwraps the mutable instance if a read-only instance has been specified.
-     * <b>Note:</b> this method is not intended to weaken the write protection of
-     * read-only instances. It is sole purpose is to return an instance that can
-     * be used for identity equality checks. Since identity is not guarantied for
-     * read-only instances, this method has to be used to unwrap read-only instances
-     * before they are added to identity hashmaps and the like.
+     * <b>Note:</b> this method is not intended to weaken the write protection
+     * of read-only instances. It is sole purpose is to return an instance that
+     * can be used for identity equality checks. Since identity is not
+     * guarantied for read-only instances, this method has to be used to unwrap
+     * read-only instances before they are added to identity hashmaps and the
+     * like.
      *
      * @param o object to unwrap
      * @return object that can be used for identity comparison
@@ -136,7 +161,8 @@ class VMFIterator implements Iterator<eu.mihosoft.vmf.runtime.core.internal.VObj
             return o;
         }
 
-        eu.mihosoft.vmf.runtime.core.internal.VObjectInternal n = (eu.mihosoft.vmf.runtime.core.internal.VObjectInternal) o;
+        eu.mihosoft.vmf.runtime.core.internal.VObjectInternal n = 
+                (eu.mihosoft.vmf.runtime.core.internal.VObjectInternal) o;
 
         // - Read-only instances have to be unwrapped since identity
         //   is not guarantied for read-only instances
@@ -158,7 +184,7 @@ class VMFIterator implements Iterator<eu.mihosoft.vmf.runtime.core.internal.VObj
     private Iterator<VObject> getCurrentIterator() {
 
         if (currentIterator == null || !currentIterator.hasNext()) {
-            if(isDebug()) {
+            if (isDebug()) {
                 System.out.println(" --> leaving current");
             }
             // obtain the last iterator that has been pushed
@@ -203,18 +229,22 @@ class VMFPropertyIterator implements Iterator<VObject> {
      * Creates a new iterator
      *
      * @param identityMap identity map that marks already visited elements
-     * @param object      model object to visit
+     * @param object model object to visit
      */
-    public VMFPropertyIterator(IdentityHashMap<Object, Object> identityMap, eu.mihosoft.vmf.runtime.core.internal.VObjectInternal object) {
+    public VMFPropertyIterator(IdentityHashMap<Object, Object> identityMap,
+            eu.mihosoft.vmf.runtime.core.internal.VObjectInternal object) {
         this.identityMap = identityMap;
         this.object = object;
 
-        if(VMFIterator.isDebug()) {
-            int numProps = object._vmf_getIndicesOfPropertiesWithModelTypeOrElementTypes().length;
+        if (VMFIterator.isDebug()) {
+            int numProps = 
+                    object._vmf_getIndicesOfPropertiesWithModelTypeOrElementTypes().length;
             System.out.println(">> prop iterator for " + object.getClass());
             for (int i = 0; i < numProps; i++) {
-                int propIndex = object._vmf_getIndicesOfPropertiesWithModelTypeOrElementTypes()[i];
-                System.out.println("  --> i: " + i + ", name: " + object._vmf_getPropertyNames()[propIndex]);
+                int propIndex = 
+                        object._vmf_getIndicesOfPropertiesWithModelTypeOrElementTypes()[i];
+                System.out.println("  --> i: " + i + ", name: "
+                        + object._vmf_getPropertyNames()[propIndex]);
             }
 
             if (numProps == 0) {
@@ -226,7 +256,7 @@ class VMFPropertyIterator implements Iterator<VObject> {
     @Override
     public boolean hasNext() {
 
-        if(VMFIterator.isDebug()) {
+        if (VMFIterator.isDebug()) {
             System.out.println(" --> checking " + index);
         }
 
@@ -244,8 +274,9 @@ class VMFPropertyIterator implements Iterator<VObject> {
                 // skip to next
                 listIterator = null;
                 // index++;
-                if(VMFIterator.isDebug()) {
-                    System.out.println("  --> leaving list, next is " + (index + 1));
+                if (VMFIterator.isDebug()) {
+                    System.out.println(
+                            "  --> leaving list, next is " + (index + 1));
                 }
             }
         }
@@ -264,7 +295,8 @@ class VMFPropertyIterator implements Iterator<VObject> {
             // iterator index
             int nextIndex = index + 1;
             int propIndex = object.
-                    _vmf_getIndicesOfPropertiesWithModelTypeOrElementTypes()[nextIndex];
+                    _vmf_getIndicesOfPropertiesWithModelTypeOrElementTypes()
+                    [nextIndex];
             Object o = object._vmf_getPropertyValueById(propIndex);
 
             // skip forward until no null element is present
@@ -282,9 +314,10 @@ class VMFPropertyIterator implements Iterator<VObject> {
             if (o instanceof VList) {
                 @SuppressWarnings("unchecked")
                 boolean hasNonEmpty = ((VList) o).stream().filter(e -> e != null).
-                        filter(e ->
-                                !identityMap.containsKey(
-                                        VMFIterator.unwrapIfReadOnlyInstanceForIdentityCheck(e))).count() > 0;
+                        filter(e
+                                -> !identityMap.containsKey(
+                                VMFIterator.unwrapIfReadOnlyInstanceForIdentityCheck(e)))
+                        .count() > 0;
 
                 if (!hasNonEmpty && index + 2 < numProperties) {
 
@@ -326,7 +359,7 @@ class VMFPropertyIterator implements Iterator<VObject> {
 
         // if list iterator is present, we return the list elements
         if (listIterator != null) {
-            if(VMFIterator.isDebug()) {
+            if (VMFIterator.isDebug()) {
                 System.out.println("  --> using list at " + index);
             }
             return listIterator.next();
@@ -334,11 +367,12 @@ class VMFPropertyIterator implements Iterator<VObject> {
 
         index++;
 
-        if(VMFIterator.isDebug()) {
+        if (VMFIterator.isDebug()) {
             System.out.println("  --> returning " + index);
         }
 
-        int propIndex = object._vmf_getIndicesOfPropertiesWithModelTypeOrElementTypes()[index];
+        int propIndex = object.
+                _vmf_getIndicesOfPropertiesWithModelTypeOrElementTypes()[index];
         Object o = object._vmf_getPropertyValueById(propIndex);
 
         // skip forward until no null element is present
@@ -349,10 +383,11 @@ class VMFPropertyIterator implements Iterator<VObject> {
         // iterating through list
         if (o instanceof VList) {
             listIterator = ((VList) o).stream().filter(e -> e != null).
-                    filter(e ->
-                            !identityMap.containsKey(
-                                    VMFIterator.unwrapIfReadOnlyInstanceForIdentityCheck(e))).iterator();
-            if(VMFIterator.isDebug()) {
+                    filter(e
+                            -> !identityMap.containsKey(
+                            VMFIterator.unwrapIfReadOnlyInstanceForIdentityCheck(e)))
+                    .iterator();
+            if (VMFIterator.isDebug()) {
                 System.out.println("  --> switching to list at " + index);
             }
             o = listIterator.next();
