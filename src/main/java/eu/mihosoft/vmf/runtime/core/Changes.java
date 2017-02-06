3e37d337ceef1eadf1519492e4bb07ca12561a2f
Michael Hoffer <info@michaelhoffer.de>
2017-02-06 22:28:49 +0100

    imroved transaction/change api

--- src/main/java/eu/mihosoft/vmf/runtime/core/Changes.java
+++ src/main/java/eu/mihosoft/vmf/runtime/core/Changes.java
@@ -5,14 +5,86 @@
  */
 package eu.mihosoft.vmf.runtime.core;
 
+import eu.mihosoft.vcollections.VList;
+import eu.mihosoft.vcollections.VMappedList;
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *
  * @author Michael Hoffer (info@michaelhoffer.de)
  */
 public interface Changes {
+
+    /**
+     * Starts recording changes.
+     */
     void start();
+
+    /**
+     * Publishes a transaction that consists of all changes since the last
+     * {@code start()} or {@code publish()} call.
+     */
+    void publish();
+
+    /**
+     * Stops recording changes. Unpublished trnsactions will be published.
+     */
     void stop();
-    
-    int numChanges();
-    
+
+    /**
+     * Returns all changes to the model (observable collection).
+     * @return all changes to the model (observable collection)
+     */
+    VList<Change> all();
+
+    /**
+     * Returns all model transactions (observable collection).
+     * @return ll model transactions (observable collection)
+     */
+    VList<Transaction> transactions();
+}
+
+class ChangesImpl implements Changes {
+
+    private final VList<Change> all = VList.newInstance(new ArrayList<>());
+    private final VList<Change> unmodifiableAll = 
+            VMappedList.newUnmodifiableInstance(all, (e)->e);
+    private final VList<Transaction> transactions
+            = VList.newInstance(new ArrayList<>());
+    private final VList<Transaction> unmodifiableTransactions
+            = VList.newInstance(Collections.unmodifiableList(transactions));
+    private int currentIndex;
+
+    @Override
+    public void start() {
+        // TODO register with model
+    }
+
+    @Override
+    public void publish() {
+        if (currentIndex < unmodifiableAll.size()) {
+            transactions.add(() -> unmodifiableAll.subList(currentIndex, all.size()));
+            currentIndex = unmodifiableAll.size();
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (currentIndex < all.size()) {
+            publish();
+        }
+        // TODO unregister from model
+    }
+
+    @Override
+    public VList<Change> all() {
+        return unmodifiableAll;
+    }
+
+    @Override
+    public VList<Transaction> transactions() {
+        return unmodifiableTransactions;
+    }
+
 }
---
+++ src/main/java/eu/mihosoft/vmf/runtime/core/Transaction.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.mihosoft.vmf.runtime.core;
+
+import java.util.List;
+
+/**
+ *
+ * @author Michael Hoffer (info@michaelhoffer.de)
+ */
+public interface Transaction {
+    List<Change> changes();
+}
+
