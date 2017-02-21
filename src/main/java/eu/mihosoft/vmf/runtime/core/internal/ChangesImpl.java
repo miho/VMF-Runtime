4729c2abc49f65826ca97f833af9f403aa180df9
Michael Hoffer <info@michaelhoffer.de>
2017-02-21 18:31:06 +0100

    Change/Undo Api seems to work forl lists

--- src/main/java/eu/mihosoft/vmf/runtime/core/internal/ChangesImpl.java
+++ src/main/java/eu/mihosoft/vmf/runtime/core/internal/ChangesImpl.java
@@ -39,33 +39,31 @@ public class ChangesImpl implements Changes {
     public void start() {
         Iterator<VObject> it = model.vmf().content().iterator();
 
-        PropertyChangeListener listener = new PropertyChangeListener() {
+        PropertyChangeListener objListener = new PropertyChangeListener() {
             @Override
             public void propertyChange(PropertyChangeEvent evt) {
-                Change c = new PropChangeImpl(model, evt.getPropertyName(),
+                Change c = new PropChangeImpl((VObject) evt.getSource(), evt.getPropertyName(),
                         evt.getOldValue(), evt.getNewValue());
                 all.add(c);
+
+                if(evt.getNewValue() instanceof VObject) {
+                    VObject newObjectToObserve = (VObject) evt.getNewValue();
+                    newObjectToObserve.removePropertyChangeListener(this);
+                    newObjectToObserve.addPropertyChangeListener(this);
+                    subscriptions.add(() -> newObjectToObserve.removePropertyChangeListener(this));
+                }
+
+                if(evt.getOldValue() instanceof VObject) {
+                    VObject objectToRemoveFromObservation = (VObject) evt.getOldValue();
+                    objectToRemoveFromObservation.removePropertyChangeListener(this);
+                }
             }
         };
 
-        model.addPropertyChangeListener(listener);
-        subscriptions.add(() -> model.removePropertyChangeListener(listener));
-
-        addListListenersToPropertiesOf(model);
-
-
         while (it.hasNext()) {
             VObject obj = it.next();
-            PropertyChangeListener objListener = new PropertyChangeListener() {
-                @Override
-                public void propertyChange(PropertyChangeEvent evt) {
-                    Change c = new PropChangeImpl(obj, evt.getPropertyName(),
-                            evt.getOldValue(), evt.getNewValue());
-                    all.add(c);
-                }
-            };
 
-            addListListenersToPropertiesOf(obj);
+            addListListenersToPropertiesOf(obj, objListener);
 
             obj.addPropertyChangeListener(objListener);
             subscriptions.add(() -> obj.removePropertyChangeListener(objListener));
@@ -73,22 +71,29 @@ public class ChangesImpl implements Changes {
     }
 
     @SuppressWarnings({"deprecation", "unchecked"})
-    private void addListListenersToPropertiesOf(VObject object) {
+    private void addListListenersToPropertiesOf(VObject object, PropertyChangeListener objListener) {
         VObjectInternalModifiable internalModel = (VObjectInternalModifiable) object;
-        List<Integer> listProperties = new ArrayList<>();
         for (int i = 0; i < internalModel._vmf_getPropertyTypes().length; i++) {
             int type = internalModel._vmf_getPropertyTypes()[i];
-
             if (type == -2) {
-                listProperties.add(i);
                 String propName = internalModel._vmf_getPropertyNames()[i];
 
                 VList<Object> list = (VList<Object>) internalModel._vmf_getPropertyValueById(i);
 
                 subscriptions.add(list.addChangeListener(
                         (evt) -> {
-                            Change c = new ListChangeImpl(object,propName, evt);
+                            Change c = new ListChangeImpl(object, propName, evt);
                             all.add(c);
+
+                            evt.added().elements().stream().filter(e->e instanceof VObject).
+                                    map(e->(VObject)e).forEach(v->
+                            {
+                                v.removePropertyChangeListener(objListener);
+                                v.addPropertyChangeListener(objListener);
+                                subscriptions.add(() -> v.removePropertyChangeListener(objListener));
+                            });
+                            evt.removed().elements().stream().filter(e->e instanceof VObject).
+                                    map(e->(VObject)e).forEach(v->v.removePropertyChangeListener(objListener));
                         }
                 ));
             }
